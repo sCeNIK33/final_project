789d970ce2050404f06175a82631cf3623363a38
spencerevanj <spencerevanj@gmail.com>
2021-03-13 12:09:56 -0600

    some updates

--- netlify/functions/get_user_posts.js
+++ netlify/functions/get_user_posts.js
@@ -1,50 +1,102 @@
 // /.netlify/functions/get_posts
 let firebase = require('./firebase')
 
+function filter(icebreaker) {
+  let filter
+  if (filter = XXXXXX) {           // Need logic here to identify the logged in users saved posts
+    filter = 'Saved'              
+  } else if (filter = XXXXXX) {     // Need logic here to identify the logged in users submitted posts
+    filter = 'Submitted'
+  } else if (filter = XXXXXX) {     // Need logic here to identify the logged in users liked posts
+    filter = 'Liked'
+  } else {
+    filter = 'Random'
+  }
+  return filter
+}
+
+function renderIceBreakers(icebreakersArray) {
+  for (let i=0; i<icebreaker.length; i++) {
+    let icebreakerId = icebreaker[i].id                                // the ID for the given post
+    let icebreakerData = icebreaker[i].data()                          // the rest of the post data
+    let icebreakerLikesQuery = await db.collection('icebreakerLikes')           // likes from Firestore
+                              .where('icebreakerId', '==', icebreakerId) // for the given postId
+                              .get()
+
+    document.querySelector('.icebreakers').insertAdjacentHTML('beforeend', `
+      <h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
+        <i class="fas fa-car-side"></i>
+        <span>${filter(icebreaker)}</span>
+      </h1>
+    `)
+
+
+    for (let i = 0; i < icebreaker.length; i++) {
+      let leg = ride[i]
+
+      document.querySelector('.icebreaker').insertAdjacentHTML('beforeend', `
+        <div class="border-4 p-4 my-4 text-left">
+          <div class="flex">
+            <div class="w-1/2">
+              <h2 class="text-2xl py-1">${leg.passengerDetails.first} ${leg.passengerDetails.last}</h2>
+              <p class="font-bold text-gray-600">${leg.passengerDetails.phoneNumber}</p>
+            </div>
+            <div class="w-1/2 text-right">
+              <span class="rounded-xl text-white p-2">
+                ${icebreakerLikesQuery} likes
+              </span>
+            </div>
+          </div>
+          <div class="mt-4 flex">
+            <div class="w-1/2">
+              <div class="text-sm font-bold text-gray-600">PICKUP</div>
+              <p>${leg.pickupLocation.address}</p>
+              <p>${leg.pickupLocation.city}, ${leg.pickupLocation.state} ${leg.pickupLocation.zip}</p>
+            </div>
+            <div class="w-1/2">
+              <div class="text-sm font-bold text-gray-600">DROPOFF</div>
+              <p>${leg.dropoffLocation.address}</p>
+              <p>${leg.dropoffLocation.city}, ${leg.dropoffLocation.state} ${leg.dropoffLocation.zip}</p>
+            </div>
+          </div>
+        </div>
+      `)
+    }
+  }
+}
+
+
 exports.handler = async function(event) {
   let db = firebase.firestore()                             // define a variable so we can use Firestore
-  let postsData = []                                        // an empty Array
+  let icebreakerData = []                                        // an empty Array
   
-  let postsQuery = await db.collection('posts')             // posts from Firestore
+  let icebreakerQuery = await db.collection('icebreakers')             // posts from Firestore
                            .orderBy('created')              // ordered by created
                            .get()
-  let posts = postsQuery.docs                               // the post documents themselves
+  let icebreaker = icebreakerQuery.docs                               // the post documents themselves
   
   // loop through the post documents
-  for (let i=0; i<posts.length; i++) {
-    let postId = posts[i].id                                // the ID for the given post
-    let postData = posts[i].data()                          // the rest of the post data
-    let likesQuery = await db.collection('likes')           // likes from Firestore
-                             .where('postId', '==', postId) // for the given postId
+  for (let i=0; i<icebreaker.length; i++) {
+    let icebreakerId = icebreaker[i].id                                // the ID for the given post
+    let icebreakerData = icebreaker[i].data()                          // the rest of the post data
+    let icebreakerLikesQuery = await db.collection('icebreakerLikes')           // likes from Firestore
+                             .where('icebreakerId', '==', icebreakerId) // for the given postId
                              .get()
-    let commentsQuery = await db.collection('comments')     // likes from Firestore
-                             .where('postId', '==', postId) // for the given postId
-                             .get()
-    let commentsData = []                                   // an empty Array
-    let comments = commentsQuery.docs                       // the comments documents
-
-    // loop through the comment documents
-    for (let i=0; i<comments.length; i++) {
-      let comment = comments[i].data()                      // grab the comment data
-      commentsData.push({
-        username: comment.username,                         // the author of the comment
-        text: comment.text                                  // the comment text
-      })
-    }
-
-    // add a new Object of our own creation to the postsData Array
-    postsData.push({
-      id: postId,                                           // the post ID
-      imageUrl: postData.imageUrl,                          // the image URL
-      username: postData.username,                          // the username
-      likes: likesQuery.size,                               // number of likes
-      comments: commentsData                                // an Array of comments
-    })
   }
   
+
+
+
+
+
   // return an Object in the format that a Netlify lambda function expects
   return {
     statusCode: 200,
     body: JSON.stringify(postsData)
   }
 }
+
+
+
+
+
