870a50e3f269a2fdb2416b1a2d4589a94ce24c18
spencerevanj <spencerevanj@gmail.com>
2021-03-16 13:44:20 -0500

    changes

--- netlify/functions/get_user_posts.js
+++ netlify/functions/get_user_posts.js
@@ -84,12 +84,6 @@ exports.handler = async function(event) {
                              .get()
   }
   
-
-
-
-
-
-  // return an Object in the format that a Netlify lambda function expects
   return {
     statusCode: 200,
     body: JSON.stringify(postsData)
