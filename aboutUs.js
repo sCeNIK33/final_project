ae0b7bff6538806eb37be3467aebde7a2e004347
Christian Nikolopoulos <cdnikolopoulos@gmail.com>
2021-03-15 19:24:20 -0500

    edited sign iin and out

--- aboutUs.js
+++ aboutUs.js
@@ -1,34 +1,33 @@
-firebase.auth().onAuthStateChanged(async function(user) {
-if (user) {
-    // Signed in
-    console.log('signed in')
-
-    document.querySelector('.sign-in-or-sign-out').innerHTML = `
-      <button class="text-pink-500 underline sign-out">Sign Out</button>
-    `
-    document.querySelector('.sign-in-or-sign-out').addEventListener('click', function(event) {
-      console.log('sign out clicked')
-      firebase.auth().signOut()
-      document.location.href = 'aboutUs.html'
-    })
-} else {
-    // Signed out
+firebase.auth().onAuthStateChanged(async function (user) {
+    if (user) {
+      // Signed in
+      console.log('signed in')
+      //sign out button
+      document.querySelector('.sign-in-or-sign-out').innerHTML = `
+  <button class="text-pink-500 underline sign-out">Sign Out</button>
+  `
+      document.querySelector('.sign-in-or-sign-out').addEventListener('click', function (event) {
+        console.log('sign out clicked')
+        firebase.auth().signOut()
+        document.location.href = 'aboutUs.html'
+      })
   
-    console.log('signed out')
-    document.querySelector('form').classList.add('hidden')
-
-    // Initializes FirebaseUI Auth
-    let ui = new firebaseui.auth.AuthUI(firebase.auth())
-
-    // FirebaseUI configuration
-    let authUIConfig = {
-      signInOptions: [
-        firebase.auth.EmailAuthProvider.PROVIDER_ID
-      ],
-      signInSuccessUrl: 'aboutUs.html'
+    } else {
+      // Signed out
+      console.log('signed out')
+  
+      // Initializes FirebaseUI Auth
+      let ui = new firebaseui.auth.AuthUI(firebase.auth())
+  
+      // FirebaseUI configuration
+      let authUIConfig = {
+        signInOptions: [
+          firebase.auth.EmailAuthProvider.PROVIDER_ID
+        ],
+        signInSuccessUrl: 'aboutUs.html'
+      }
+  
+      // Starts FirebaseUI Auth
+      ui.start('.sign-in-or-sign-out', authUIConfig)
     }
-
-    // Starts FirebaseUI Auth
-    ui.start('.sign-in-or-sign-out', authUIConfig)
-  }
-})
+  })
