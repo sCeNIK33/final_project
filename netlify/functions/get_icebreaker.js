132e11f0def153f9c599c538ec95810671ecc6e2
spencerevanj <spencerevanj@gmail.com>
2021-03-16 18:40:47 -0500

    more updates

--- netlify/functions/get_icebreaker.js
+++ netlify/functions/get_icebreaker.js
@@ -6,36 +6,24 @@ let firebase = require('./firebase')
 exports.handler = async function(event) {
     let queryStringUserId = event.queryStringParameters.userId
 
-  let icebreakerData = []                                      
-  let db = firebase.firestore()
+    let icebreakerData = []                                      
+    let db = firebase.firestore()
 
-  let querySnapshot = await db.collection('icebreaker')             // posts from Firestore
-                           .where('userId', "==", queryStringUserId)              // ordered by created
-                           .get()
-  let icebreakers = querySnapshot.docs                               // the post documents themselves
-  
-  // loop through the post documents
-  for (let i=0; i<icebreakers.length; i++) {
-    let icebreakerId = icebreakers[i].id                         
-    let icebreaker = icebreakers[i].data()                         
-    let likesQuery = await db.collection('likes')           
-                             .where('icebreakerId', '==', icebreakerId) 
-                             .get()
-
-    icebreakerData.push({
-      id: icebreakerId,
-      text: icebreaker.text
-    })
-  }
-
-    // add a new Object of our own creation to the postsData Array
-  //   icebreakerData.push({
-  //     id: icebreakerId,                                           // the post ID
-  //     text: icebreaker.text,                          // the image URL
-  //     username: icebreakerData.username,                          // the username
-  //     likes: likesQuery.size,                               // number of likes                                // an Array of comments
-  //   })
-  // }
+    let querySnapshot = await db.collection('icebreaker')             // posts from Firestore
+                            .where('userId', "==", queryStringUserId)              // ordered by created
+                            .get()
+    let icebreakers = querySnapshot.docs                               // the post documents themselves
+    
+    // loop through the post documents
+    for (let i=0; i<icebreakers.length; i++) {
+      let icebreakerId = icebreakers[i].id                         
+      let icebreaker = icebreakers[i].data()                         
+    
+      icebreakerData.push({
+        id: icebreakerId,
+        text: icebreaker.text
+      })
+    }
   
   // return an Object in the format that a Netlify lambda function expects
   return {
