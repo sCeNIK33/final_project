f67bd1b3e1220aaae8a6bb460bb93a0cfc0bb978
spencerevanj <spencerevanj@gmail.com>
2021-03-16 18:16:32 -0500

    updates

--- account.js
+++ account.js
@@ -2,6 +2,59 @@ firebase.auth().onAuthStateChanged(async function (user) {
     if (user) {
       // Signed in
       console.log('signed in')
+
+      let response = await fetch(`/.netlify/functions/get_icebreaker?userId=${user.uid}`)
+      let icebreakers = await response.json()
+      console.log(icebreakers)
+
+      for (let i=0; i<icebreakers.length; i++) {
+        let icebreaker = icebreakers[i]
+        let icebreakerId = icebreaker.id
+        let icebreakerText = icebreaker.text
+
+        document.querySelector('.icebreaker').insertAdjacentHTML('beforeend', `
+          <div class="icebreaker-${icebreakerId} py-4 text-xl border-b-2 border-purple-500 w-full">
+            <a href="#" class="done p-2 text-sm bg-green-500 text-white">✓</a>
+            ${icebreakerText}
+            <a href="#" class="like-button block text-center text-white bg-green-500 mt-4 px-4 py-2 rounded">Liked</a>
+            <a href="#" class="used-button block text-center text-white bg-green-500 mt-4 px-4 py-2 rounded">Used</a>
+          </div>
+        `)
+
+        document.querySelector(`.icebreaker-${icebreakerId} .like-button`).addEventListener('click', async function(event) {
+          event.preventDefault()
+          document.querySelector(`.icebreaker-${icebreakerId}`).classList.add('opacity-20')
+        
+            // make fetch POST request to backend to delete a used icebreaker
+          await fetch('/.netlify/functions/used_icebreaker', {
+            method: 'POST',
+            body: JSON.stringify({
+              icebreakerId: icebreaker.id
+            })
+          })  
+        })
+      
+
+        document.querySelector(`.icebreaker-${icebreakerId} .used-button`).addEventListener('click', async function(event) {
+          event.preventDefault()
+          document.querySelector(`.icebreaker-${icebreakerId}`).classList.add('opacity-20')
+
+          // make fetch POST request to backend to delete a liked icebreaker
+          await fetch('/.netlify/functions/like', {
+            method: 'POST',
+            body: JSON.stringify({
+              icebreakerId: icebreaker.id
+            })
+          })
+        })
+      }
+
+     
+
+
+
+
+
       //sign out button
       document.querySelector('.sign-in-or-sign-out').innerHTML = `
   <button class="text-pink-500 underline sign-out">Sign Out</button>
